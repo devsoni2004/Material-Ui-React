3a3fb7d36f263e3ad7eac05fcf52004ea17ef803
devsoni2004 <vipbrothers92@gmail.com>
2023-06-08 16:45:55 +0530

    Filter Table Degin

--- src/Pages/Merchants/MerchantsList.js
+++ src/Pages/Merchants/MerchantsList.js
@@ -11,6 +11,15 @@ import { useState, useEffect } from 'react'
 import axios from 'axios';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import TextField from "@mui/material/TextField";
+import Autocomplete from "@mui/material/Autocomplete";
+import Divider from "@mui/material/Divider";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import Typography from "@mui/material/Typography";
+import Swal from "sweetalert2";
+import Box from "@mui/material/Box";
 import Splitbtn from '../../Components/Splitbtn';
 const columns = [
     { id: 'name', label: 'Merchant Id', minWidth: 170 },
@@ -43,7 +52,7 @@ const columns = [
         align: 'right',
         format: (value) => value.toLocaleString('en-US'),
     },
-   
+
 ];
 
 function createData(name, code, population, size) {
@@ -81,13 +90,62 @@ export default function MerchantsList() {
                 console.log(error);
             });
     }, []);
+
+    const deleteUser = (singhtek_id) => {
+        Swal.fire({
+          title: "Are you sure?",
+          text: "You won't be able to revert this!",
+          icon: "warning",
+          showCancelButton: true,
+          confirmButtonColor: "#3085d6",
+          cancelButtonColor: "#d33",
+          confirmButtonText: "Yes, delete it!",
+        }).then((result) => {
+          if (result.value) {
+            // deleteApi(singhtek_id);
+          }
+        });
+      };
+    
     return (
         <Paper sx={{ width: '80%', overflow: 'hidden', marginLeft: '60px' }}>
+                <Typography
+            gutterBottom
+            variant="h5"
+            component="div"
+            sx={{ padding: "20px" }}
+          >
+            All Merchants Data
+          </Typography>
+          <Divider />
+          <Box height={10} />
+          <Stack direction="row" spacing={2} className="my-2 mb-2">
+            <Autocomplete
+              disablePortal
+              id="combo-box-demo"
+              options={merchants}
+              sx={{ width: 300 }}
+             // onChange={(e, v) => filterData(v)}
+              getOptionLabel={(merchants) => merchants.item.user_name || ""}
+              renderInput={(params) => (
+                <TextField {...params} size="small" label="Search Merchants" />
+              )}
+            />
+            <Typography
+              variant="h6"
+              component="div"
+              sx={{ flexGrow: 1 }}
+            ></Typography>
+            <Button variant="contained" endIcon={<AddCircleIcon />}>
+              Add
+            </Button>
+          </Stack>
+          <Box height={10} />
             <TableContainer sx={{ maxHeight: 440 }}>
                 <Table stickyHeader aria-label="sticky table">
-                    <TableHead className="thead">
+                    <TableHead>
                         <TableRow>
-                            {columns.map((column) => (
+                            {/* {columns.map((column) => (
                                 <TableCell
                                     key={column.id}
                                     align={column.align}
@@ -95,7 +153,28 @@ export default function MerchantsList() {
                                 >
                                     {column.label}
                                 </TableCell>
-                            ))}
+                            ))} */}
+                            <TableCell align="left" style={{ minWidth: "100px" }}>
+                                Merchant Id
+                            </TableCell>
+                            <TableCell align="left" style={{ minWidth: "100px" }}>
+                                Merchant Name
+                            </TableCell>
+                            <TableCell align="left" style={{ minWidth: "100px" }}>
+                                Email
+                            </TableCell>
+                            <TableCell align="left" style={{ minWidth: "100px" }}>
+                                Mobile No.
+                            </TableCell>
+                            <TableCell align="left" style={{ minWidth: "100px" }}>
+                                Status
+                            </TableCell>
+                            <TableCell align="left" style={{ minWidth: "100px" }}>
+                                View Transaction
+                            </TableCell>
+                            <TableCell align="left" style={{ minWidth: "100px" }}>
+                                Action
+                            </TableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody className="tdata">
@@ -123,9 +202,32 @@ export default function MerchantsList() {
                                         </Stack>
                                     </TableCell>
                                     <TableCell align="left">
-                                    <Stack direction="row">
-                                        <Button variant="contained" >View All</Button>
-                                    </Stack>
+                                        <Stack direction="row">
+                                            <Button variant="contained" >View All</Button>
+                                        </Stack>
+                                    </TableCell>
+                                    <TableCell align="left">
+                                        <Stack spacing={2} direction="row">
+                                            <EditIcon
+                                                style={{
+                                                    fontSize: "20px",
+                                                    color: "blue",
+                                                    cursor: "pointer",
+                                                }}
+                                                className="cursor-pointer"
+                                            // onClick={() => editUser(row.id)}
+                                            />
+                                            <DeleteIcon
+                                                style={{
+                                                    fontSize: "20px",
+                                                    color: "darkred",
+                                                    cursor: "pointer",
+                                                }}
+                                                onClick={() => {
+                                                    deleteUser(item.singhtek_id);
+                                                }}
+                                            />
+                                        </Stack>
                                     </TableCell>
                                 </TableRow>
                             )

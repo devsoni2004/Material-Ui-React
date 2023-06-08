6242999cdd5fa669dd3ab1e6b15d889dd4dd3fff
devsoni2003 <rksonipatan232@gmail.com>
2023-06-08 05:52:52 -0700

    Search Filter Update

--- src/Pages/Merchants/MerchantsList.js
+++ src/Pages/Merchants/MerchantsList.js
@@ -14,13 +14,14 @@ import Button from '@mui/material/Button';
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
 import TextField from "@mui/material/TextField";
-import Autocomplete from "@mui/material/Autocomplete";
+import { Modal, ModalBody, ModalHeader } from 'reactstrap'
 import Divider from "@mui/material/Divider";
 import AddCircleIcon from "@mui/icons-material/AddCircle";
 import Typography from "@mui/material/Typography";
 import Swal from "sweetalert2";
 import Box from "@mui/material/Box";
 import Splitbtn from '../../Components/Splitbtn';
+
 const columns = [
     { id: 'name', label: 'Merchant Id', minWidth: 170 },
     { id: 'code', label: 'Merchant Name', minWidth: 100 },
@@ -65,7 +66,12 @@ function createData(name, code, population, size) {
 export default function MerchantsList() {
     const [page, setPage] = React.useState(0);
     const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
     const [merchants, setmerchants] = useState([]);
+    const [search, setSearch] = useState('');
+    const [currentMerchant, setCurrentMerchant] = useState(null);
+    const [modal, setmodal] = useState(false);
+    const [view, setview] = useState(false);
     const handleChangePage = (event, newPage) => {
         setPage(newPage);
     };
@@ -93,54 +99,63 @@ export default function MerchantsList() {
 
     const deleteUser = (singhtek_id) => {
         Swal.fire({
-          title: "Are you sure?",
-          text: "You won't be able to revert this!",
-          icon: "warning",
-          showCancelButton: true,
-          confirmButtonColor: "#3085d6",
-          cancelButtonColor: "#d33",
-          confirmButtonText: "Yes, delete it!",
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!",
         }).then((result) => {
-          if (result.value) {
-            // deleteApi(singhtek_id);
-          }
+            if (result.value) {
+                // deleteApi(singhtek_id);
+            }
         });
-      };
-    
+    };
+
+    const viewAll = (item) => {
+        setCurrentMerchant(item);
+        setview(true);
+    }
+
     return (
+
         <Paper sx={{ width: '80%', overflow: 'hidden', marginLeft: '60px' }}>
-                <Typography
-            gutterBottom
-            variant="h5"
-            component="div"
-            sx={{ padding: "20px" }}
-          >
-            All Merchants Data
-          </Typography>
-          <Divider />
-          <Box height={10} />
-          <Stack direction="row" spacing={2} className="my-2 mb-2">
-            <Autocomplete
-              disablePortal
-              id="combo-box-demo"
-              options={merchants}
-              sx={{ width: 300 }}
-             // onChange={(e, v) => filterData(v)}
-              getOptionLabel={(merchants) => merchants.item.user_name || ""}
-              renderInput={(params) => (
-                <TextField {...params} size="small" label="Search Merchants" />
-              )}
-            />
+
             <Typography
-              variant="h6"
-              component="div"
-              sx={{ flexGrow: 1 }}
-            ></Typography>
-            <Button variant="contained" endIcon={<AddCircleIcon />}>
-              Add
-            </Button>
-          </Stack>
-          <Box height={10} />
+                gutterBottom
+                variant="h5"
+                component="div"
+                sx={{ padding: "20px" }}
+            >
+                All Merchants Data
+            </Typography>
+            <Box
+                component="form"
+                sx={{
+                    '& > :not(style)': { m: 1, width: '25ch' },
+                }}
+                noValidate
+                autoComplete="off"
+
+            >
+                <TextField id="outlined-basic" label="Search" variant="outlined" onChange={(e) => setSearch(e.target.value)} />
+
+            </Box>
+            <Divider />
+            <Box height={10} />
+            <Stack direction="row" spacing={2} className="my-2 mb-2">
+
+                <Typography
+                    variant="h6"
+                    component="div"
+                    sx={{ flexGrow: 1 }}
+                ></Typography>
+                <Button variant="contained" endIcon={<AddCircleIcon />}>
+                    Add
+                </Button>
+            </Stack>
+            <Box height={10} />
             <TableContainer sx={{ maxHeight: 440 }}>
                 <Table stickyHeader aria-label="sticky table">
                     <TableHead>
@@ -179,59 +194,64 @@ export default function MerchantsList() {
                     </TableHead>
                     <TableBody className="tdata">
                         {
-                            merchants.map(item => (
-
-                                <TableRow hover role="checkbox tabindex={-1}">
-                                    <TableCell key={item.singhtek_id} align="left">
-                                        {item.singhtek_id}
-                                    </TableCell>
-                                    <TableCell key={item.user_name} align="left">
-                                        {item.user_name}
-                                    </TableCell>
-                                    <TableCell key={item.email} align="left">
-                                        {item.email}
-                                    </TableCell>
-                                    <TableCell key={item.mobile} align="left">
-                                        {item.mobile}
-                                    </TableCell>
-                                    <TableCell align="left">
-                                        <Stack direction="row" className="btn-outer">
-                                            <Button>
-                                                <Splitbtn />
-                                            </Button>
-                                        </Stack>
-                                    </TableCell>
-                                    <TableCell align="left">
-                                        <Stack direction="row">
-                                            <Button variant="contained" >View All</Button>
-                                        </Stack>
-                                    </TableCell>
-                                    <TableCell align="left">
-                                        <Stack spacing={2} direction="row">
-                                            <EditIcon
-                                                style={{
-                                                    fontSize: "20px",
-                                                    color: "blue",
-                                                    cursor: "pointer",
-                                                }}
-                                                className="cursor-pointer"
-                                            // onClick={() => editUser(row.id)}
-                                            />
-                                            <DeleteIcon
-                                                style={{
-                                                    fontSize: "20px",
-                                                    color: "darkred",
-                                                    cursor: "pointer",
-                                                }}
-                                                onClick={() => {
-                                                    deleteUser(item.singhtek_id);
-                                                }}
-                                            />
-                                        </Stack>
-                                    </TableCell>
-                                </TableRow>
-                            )
-                            )
+                            merchants
+                                .filter((item) => {
+                                    return search.toLowerCase() === ''
+                                        ? item
+                                        : item.user_name.toLowerCase().includes(search);
+                                })
+                                .map(item => (
+                                    <TableRow hover role="checkbox tabindex={-1}">
+                                        <TableCell key={item.singhtek_id} align="left">
+                                            {item.singhtek_id}
+                                        </TableCell>
+                                        <TableCell key={item.user_name} align="left">
+                                            {item.user_name}
+                                        </TableCell>
+                                        <TableCell key={item.email} align="left">
+                                            {item.email}
+                                        </TableCell>
+                                        <TableCell key={item.mobile} align="left">
+                                            {item.mobile}
+                                        </TableCell>
+                                        <TableCell align="left">
+                                            <Stack direction="row" className="btn-outer">
+                                                <Button>
+                                                    <Splitbtn />
+                                                </Button>
+                                            </Stack>
+                                        </TableCell>
+                                        <TableCell align="left">
+                                            <Stack direction="row">
+                                                <Button variant="contained" onClick={() => viewAll(true)}>View All</Button>
+                                            </Stack>
+                                        </TableCell>
+                                        <TableCell align="left">
+                                            <Stack spacing={2} direction="row">
+                                                <EditIcon
+                                                    style={{
+                                                        fontSize: "20px",
+                                                        color: "blue",
+                                                        cursor: "pointer",
+                                                    }}
+                                                    className="cursor-pointer"
+                                                // onClick={() => editUser(row.id)}
+                                                />
+                                                <DeleteIcon
+                                                    style={{
+                                                        fontSize: "20px",
+                                                        color: "darkred",
+                                                        cursor: "pointer",
+                                                    }}
+                                                    onClick={() => {
+                                                        deleteUser(item.singhtek_id);
+                                                    }}
+                                                />
+                                            </Stack>
+                                        </TableCell>
+                                    </TableRow>
+                                )
+                                )
                         }
                     </TableBody>
                 </Table>

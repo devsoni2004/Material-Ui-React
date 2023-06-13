ceff971e49ae1f3afb18796527f0803dd4632f22
omappdeveloper <omappdeveloper@gmail.com>
2023-06-13 15:30:08 +0530

    dashboard route added as root

--- src/Components/ProtectedRoute.js
+++ src/Components/ProtectedRoute.js
@@ -21,6 +21,7 @@ const ProtectedRoute = () => {
                             <Outlet />
                         </Box>
                     }>
+                        <Route path={pages.ROOT} exact element={<Dashboard />}></Route>
                         <Route path={pages.DASHBOARD} exact element={<Dashboard />}></Route>
                         <Route path={pages.ALL_MERCHANTS_DATA} exact element={<AllMerchants />}></Route>
                         <Route path={pages.ACCEPT_USERS} exact element={<AcceptUser />}></Route>
